45fcc6dd1a69e42fa7230bb02bc70ad546a61d9c
GianatiempoJulian <gianatiempojulian@gmail.com>
2024-02-26 23:09:16 -0300

    Added getByID and getByEmail for my cuchurrumin

--- server/controllers/userController.mjs
+++ server/controllers/userController.mjs
@@ -32,6 +32,41 @@ export const getAll = async function(req, res) {
     }
 }
 
+//* Obtener usuario por id *//
+export const getByID = async function(req, res) {
+    try{
+        if(req.body)
+        {
+            const [user] = await connection.query(
+                `SELECT * FROM user WHERE id =
+                (?);`
+                [req.body.id]
+              )
+              res.send(user)
+        }
+    }catch(err){
+        console.error(err)
+    }
+}
+
+//* Obtener usuario por email *//
+export const getByEmail = async function(req, res) {
+    try{
+        if(req.body)
+        {
+            const [user] = await connection.query(
+                `SELECT * FROM user WHERE email =
+                (?);`
+                [req.body.email]
+              )
+              res.send(user)
+        }
+    }catch(err){
+        console.error(err)
+    }
+}
+
+
 //* Eliminar usuario *//
 export const remove = async function(req, res) {
     try{
