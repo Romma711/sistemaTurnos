8a52d9400fcc4f1cd305c9a54db018ee61770cea
GianatiempoJulian <gianatiempojulian@gmail.com>
2024-02-28 21:25:55 -0300

    Add reservation model

---
+++ server/models/reservation.mjs
@@ -0,0 +1,63 @@
+import { connection } from '../db/connect.mjs';
+
+export class reservationModel{
+    //* Crear reserva *//
+    static async create ({ input }) {
+        const {
+            idAppointment,
+            idUser
+          } = input
+        try
+        {
+            await connection.query(`INSERT INTO reservation (idAppointment, idUser)
+                VALUES (?, ?);`,
+                [idAppointment, idUser])
+        }catch(err){
+            if(err.sqlState == 23000)
+            {
+                console.error(err)
+            }
+        }
+    }
+    
+    //* Obtener todas las reservas *//
+    static async getAll () {
+        try{
+            const [reservations] = await connection.query(
+                'SELECT * FROM reservation'
+                )
+               return reservations
+        }
+        catch(err){
+            console.error(err)
+        }
+    }
+    
+    //* Obtener reserva por id *//
+    static async getByID ({ id }) {
+        try{
+            const [user] = await connection.query(
+                `SELECT * FROM reservation WHERE id =
+                (?);`
+                [id]
+                )
+                return user[0]
+        }
+        catch(err){
+            console.error(err)
+        }
+    }
+    
+    //* Eliminar reserva *//
+    static async remove({ id }) {
+        try{
+            await connection.query(`DELETE FROM reservation WHERE id =
+            (?);`,
+            [id])
+        }catch(err){
+            console.error(err)
+        }
+    }
+    
+    
+}
